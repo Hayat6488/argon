ea051074ab2b444d4d30e0077befd0ff914dd775
Hayat6488 <abulhayat619@gmail.com>
2023-01-21 13:26:18 +0600

    reset

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -7,21 +7,6 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    // apiKey: "AIzaSyDngtYQkyY3SAsnoNMmMdcfTqstkntcY_Y",
-    // authDomain: "argon-app-f3026.firebaseapp.com",
-    // projectId: "argon-app-f3026",
-    // storageBucket: "argon-app-f3026.appspot.com",
-    // messagingSenderId: "339819060556",
-    // appId: "1:339819060556:web:3332d245a469e33ac2c3e9"
-
-    // apiKey: "AIzaSyD22e-M8WbUsanypvGQ1YSO6ZiU917i868",
-    // authDomain: "argon-1d04c.firebaseapp.com",
-    // projectId: "argon-1d04c",
-    // storageBucket: "argon-1d04c.appspot.com",
-    // messagingSenderId: "566194160028",
-    // appId: "1:566194160028:web:44554ce7b4ef5481a60859"
-
-    // Main DataBase
 
     apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
