5d45bbe57cfa1e6b99e576e6db5111cbca22fbba
Hayat6488 <abulhayat619@gmail.com>
2023-01-25 23:58:07 +0600

    transactions done

--- src/views/pages/Transactions/Transactions.js
+++ src/views/pages/Transactions/Transactions.js
@@ -7,42 +7,119 @@ import {
   CardHeader,
   Row,
   Col,
-  Table
+  Table,
+  Button
 } from "reactstrap";
-import { onSnapshot, collection } from "firebase/firestore";
+import { onSnapshot, collection, getDocs } from "firebase/firestore";
 import SimpleHeader from "components/Headers/SimpleHeader";
 import { db } from "Firebase/firebase.config";
+import Modals from "./Modal/Modals";
 
 function Transactions() {
 
+  const [exampleModal, setExampleModal] = React.useState(false)
+  const [transactioDetails, setTransactioDetails] = React.useState(null)
+  const [amount, setAmount] = React.useState(null)
 
-  // const collectionRef = collection(db, "transactions");
+  const openModal = (transaction) => {
+    setExampleModal(!exampleModal);
+    setTransactioDetails(transaction);
+    setAmount(transaction?.amount);
+  }
 
   const [transactions, setTransactions] = React.useState([]);
+  const [users, setUsers] = React.useState([]);
+  const [providers, setProviders] = React.useState([]);
   const [loading, setLoading] = React.useState(false);
+  const [serviceFee, setServiceFee] = React.useState([]);
 
-  // const q = query();
+  const userRef = collection(db, "/usersList/user/children");
+  const tradesmanRef = collection(db, "/usersList/provider/children");
 
-  // React.useLayoutEffect(() => {
-  //   const unSub = onSnapshot(collectionRef, (QuerySnapshot) => {
-  //     const items = [];
-  //     QuerySnapshot.forEach((doc) => {
+  
+  const serviceFeeRef = collection(db, "serviceFee");
 
-  //       items.push({ _id: doc.id, ...doc.data() });
-  //     });
-  //     setTransactions(items);
-  //     setLoading(false);
-  //   });
+  React.useLayoutEffect(() => {
+      const unSub = onSnapshot(serviceFeeRef, (QuerySnapshot) => {
+          const items = [];
+          QuerySnapshot.forEach((doc) => {
 
-  //   return () => {
-  //     unSub();
-  //   };
-  // }, [collectionRef])
+              items.push({ id: doc.id, ...doc.data() });
+          });
+          setServiceFee(items);
+          setLoading(false);
+      });
 
+      return () => {
+          unSub();
+      };
+  }, [])
+  
 
-  // const Delete = (id) => {
-  //   console.log(id);
-  // }
+  React.useLayoutEffect(() => {
+    const unSub = onSnapshot(userRef, (QuerySnapshot) => {
+      const items = [];
+      QuerySnapshot.forEach((doc) => {
+
+        items.push({ uid: doc.id, ...doc.data() });
+      });
+      setUsers(items);
+      setLoading(false);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, []);
+
+  React.useLayoutEffect(() => {
+    const unSub = onSnapshot(tradesmanRef, (QuerySnapshot) => {
+      const items = [];
+      QuerySnapshot.forEach((doc) => {
+
+        items.push({ uid: doc.id, ...doc.data() });
+      });
+      setProviders(items);
+      setLoading(false);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, []);
+
+  React.useLayoutEffect(() => {
+    const getData = async () => {
+      const querySnapshot = await getDocs(collection(db, "transactions"));
+      let dataList = [];
+
+      querySnapshot.forEach(async (x) => {
+        const userId = x.data().userUID;
+        const providerId = x.data().providerUID;
+
+        const user = users?.find(user => user.uid === userId);
+        const provider = providers?.find(provider => provider.uid === providerId);
+        
+        dataList.push({
+          id: x.id,
+          ...x.data(),
+          ...user,
+          ...{provider},
+        });
+      });
+      setTransactions(dataList);
+      setLoading(false)
+    };
+    getData();
+  }, [users, providers, serviceFee]);
+
+ const time = (date) => {
+  const formatDate = new Date(
+    date.seconds * 1000 + date.nanoseconds / 1000000
+  );
+  return formatDate.toLocaleTimeString('en-us', { day: 'numeric', month: 'long', });
+ }
+
 
   if (loading) {
     return <h1>Loading</h1>
@@ -57,7 +134,7 @@ function Transactions() {
             <CardHeader className="border-0">
               <Row>
                 <Col xs="6">
-                  <h3 className="mb-0">POSTS</h3>
+                  <h3 className="mb-0">TRANSACTIONS</h3>
                 </Col>
               </Row>
             </CardHeader>
@@ -75,44 +152,41 @@ function Transactions() {
               </thead>
               <tbody>
                 {
-                  transactions.map(transaction => <tr key={transaction._id}>
+                  transactions.map(transaction => <tr key={transaction?._id}>
                     <th scope="row">
-                      <img className="avatar rounded-circle" alt="..." src={require("assets/img/theme/team-4.jpg").default} />
-                      <span className="mb-0 text-sm">
-                        {transaction.user}
+                      <img className="avatar rounded-circle" alt="..." src={transaction?.photURL} />
+                      <span className="mb-0 ml-2 text-sm">
+                        {transaction?.name}
                       </span>
                     </th>
                     <td>
-                    <img className="avatar rounded-circle" alt="..." src={require("assets/img/theme/team-4.jpg").default} />
-                      <span>
-                        {transaction.tradesman}
+                      <img className="avatar rounded-circle" alt="..." src={transaction?.provider?.profilePhoto} />
+                      <span className="ml-2 text-sm">
+                        {transaction?.provider?.name}
                       </span>
                     </td>
                     <td>
+                      <span color="" className="badge-dot mr-2">
+                        {transaction?.amount}
+                      </span>
+                      <Button  
+                      className="px-1"
+                      color="secondary"
+                      outline
+                      type="button"
+                      onClick={() => openModal(transaction)}>
+                      <i className="fas fa-eye" />
+                      </Button>
+                    </td>
+                    <td>
                       <Badge color="" className="badge-dot mr-4">
-                        {/* <i className="bg-warning" /> */}
-                        {transaction.amount}
+                        {time(transaction?.time)}
                       </Badge>
                     </td>
                     <td>
                       <Badge color="" className="badge-dot mr-4">
-                        {transaction.time}
+                        {transaction?.trxID}
                       </Badge>
-                    </td>
-                    <td>
-                      <Badge color="" className="badge-dot mr-4">
-                        {transaction.trxID}
-                      </Badge>
-                    </td>
-                    <td className="table-actions">
-                      <button
-                        aria-label="Close"
-                        className="close"
-                        data-dismiss="modal"
-                        type="button"
-                      >
-                        <span aria-hidden={true}><i className="fas fa-eye" /></span>
-                      </button>
                     </td>
                   </tr>)
                 }
@@ -120,6 +194,9 @@ function Transactions() {
             </Table>
           </Card>
         </Container>
+        {
+          exampleModal && <Modals key={transactioDetails?._id} serviceFee={serviceFee} setExampleModal={setExampleModal} exampleModal={exampleModal} amount={amount}></Modals>
+        }
       </>
     );
   }
